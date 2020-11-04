35f0a00d4d5d513959fad5a52f14f515461e1250
Sabrina Bertol <bertol.sabrina@gmail.com>
2020-11-04 00:22:16 +0000

    Finished task manager challenge

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    @task.save
+    redirect_to task_path(@task.id)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
